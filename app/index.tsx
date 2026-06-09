530c21c7672aabc78bf7c545159f864c34aeffce
Daniel Borges <dborgesgodinho@gmail.com>
2026-06-09 00:20:23 -0300

    Projeto base

--- app/index.tsx
+++ app/index.tsx
@@ -14,7 +14,7 @@ export default function Index() {
 
 const styles = StyleSheet.create({
     container : {
-        flex: 1,
+        flex 1,
         justifyContent: "center",
         alignItems: "center"
     },
